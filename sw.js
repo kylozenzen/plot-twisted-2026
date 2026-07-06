1d6dd0dda58d1cdf17d562a9fe19abb8e92916da
Ben Campbell <hirebencampbell@gmail.com>
2026-07-05 22:37:25 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'plot-twisted-v1';
+const ASSETS = [
+  './', './index.html', './manifest.webmanifest',
+  './icon-192.png', './icon-512.png', './icon-maskable-512.png',
+  './apple-touch-icon-180.png', './og-image.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    caches.match(req).then(hit => hit || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => { try { c.put(req, copy); } catch (_) {} });
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
